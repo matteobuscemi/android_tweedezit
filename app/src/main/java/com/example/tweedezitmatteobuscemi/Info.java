25485b17f5fd44374fbf78e19a6415e162777b55
matteobuscemi <43605898+matteobuscemi@users.noreply.github.com>
2021-08-17 15:14:54 +0200

    home pagina klaar, info pagina wip

--- app/src/main/java/com/example/tweedezitmatteobuscemi/Info.java
+++ app/src/main/java/com/example/tweedezitmatteobuscemi/Info.java
@@ -3,18 +3,27 @@ package com.example.tweedezitmatteobuscemi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Info extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class Info extends AppCompatActivity {
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        myDialog = new Dialog(this);
+
 
         //Initialize And Assing Variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -26,11 +35,11 @@ public class Info extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.home:
                         startActivity(new Intent(getApplicationContext()
-                                ,MainActivity.class));
-                        overridePendingTransition(0,0);
+                                , MainActivity.class));
+                        overridePendingTransition(0, 0);
                         return true;
 
                     case R.id.info:
@@ -38,12 +47,25 @@ public class Info extends AppCompatActivity {
 
                     case R.id.contact:
                         startActivity(new Intent(getApplicationContext()
-                                ,Contact.class));
-                        overridePendingTransition(0,0);
+                                , Contact.class));
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
         });
+    }
+    public void ShowPopup (View v) {
+        TextView txtclose;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.show();
+
     }
 }
